076b4a79da39586a2eb074e51dc01435171ddd32
Saif Jamal <saifjamal4649@gmail.com>
2020-03-20 02:04:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,7 +195,7 @@ def player_stats(players_name)
       if attributes == :players
         data.each do |player|
           if player[:player_name] == players_name
-            new_hash = 
+            new_hash = player.delete_if do 
 end
 
 
